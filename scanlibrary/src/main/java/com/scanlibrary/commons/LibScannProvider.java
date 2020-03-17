de610c30f405b06cfe42c61c5324eb34a8bdc97b
César Cano <cesarcanojmz@gmail.com>
2020-03-17 16:38:16 -0600

    Fis - FileProvider Name

---
+++ scanlibrary/src/main/java/com/scanlibrary/commons/LibScannProvider.java
@@ -0,0 +1,11 @@
+package com.scanlibrary.commons;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * Created by cecano@compartamos.com
+ * on 17/03/2020.
+ */
+public class LibScannProvider extends FileProvider {
+    // Used for providerr
+}
